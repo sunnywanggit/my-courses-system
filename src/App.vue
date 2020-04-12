84b79ebc722a9a64748ddbbe1d9ba69d6543a7a0
ergouwang <mrergouwang@gmail.com>
2020-04-12 11:31:17 +0800

    在App created钩子函数中手动存储用户登录信息，防止页面刷新重新登录

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,24 @@
   </div>
 </template>
 
+<script lang="ts">
+  import {Component,Vue} from 'vue-property-decorator'
+  import {State, Getter, Mutation, Action} from "vuex-class";
+
+
+  @Component({
+    components: {}
+  })
+  export default class App extends Vue{
+    //存储用户信息
+    @Action('setUser') setUser:any;
+
+    created(): void {
+      this.setUser(localStorage.tsToken)
+    }
+  }
+</script>
+
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,8 +1,13 @@
 import {ActionTree} from "vuex";
+import jwt_decode from 'jwt-decode'
+
 
 const  actions:ActionTree<any,any>={
     async setUser({state,commit},user:any){
-        // console.log(user);
+        let jwtDecode = jwt_decode(user)
+        // console.log(jwtDecode);
+        commit('SET_USER',jwtDecode)
+
     }
 }
 
